a467c9efcbae89b7e3eba7b7d9cd0aa87db68093
hazrath <hazrath.autospark74@hotmail.com>
2023-09-21 13:02:37 +0600

    rect form created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
+import Form from "./components/Form";
 function App() {
   return (
-    <div className="App"> 
-
-    </div>
+    <Form />
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Form() {
+    return (
+        <>
+      <form>
+        <label>Enter your name:
+          <input type="text" value="JavaScript" placeholder="Enter text" />
+        </label>
+      </form>
+      </>
+    )
+  }
+
+  export default Form;
